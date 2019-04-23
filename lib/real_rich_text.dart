d6f2f8ea664c325b0dc5545712ec63d33dd867d9
zdwchang <zdwchang@gmail.com>
2019-04-23 14:47:39 +1000

    extract imagespan out of nested children to be displayable

--- lib/real_rich_text.dart
+++ lib/real_rich_text.dart
@@ -63,6 +63,17 @@ class RealRichText extends Text {
             maxLines: maxLines,
             locale: locale);
 
+  List<TextSpan> extractAllNestedChildren(TextSpan textSpan) {
+    if (textSpan.children == null || textSpan.children.length == 0) {
+      return [textSpan];
+    }
+    List<TextSpan> childrenSpan = [];
+    textSpan.children.forEach((child) {
+      childrenSpan.addAll(extractAllNestedChildren(child));
+    });
+    return childrenSpan;
+  }
+
   @override
   Widget build(BuildContext context) {
     final DefaultTextStyle defaultTextStyle = DefaultTextStyle.of(context);
@@ -74,10 +85,13 @@ class RealRichText extends Text {
           .merge(const TextStyle(fontWeight: FontWeight.bold));
 
     TextSpan textSpan = TextSpan(
-      style: effectiveTextStyle,
-      text: "",
-      children: textSpanList,
-    );
+        style: effectiveTextStyle,
+        text: "",
+        children: extractAllNestedChildren(TextSpan(
+          style: effectiveTextStyle,
+          text: "",
+          children: textSpanList,
+        )));
 
     // pass the context to ImageSpan to create a ImageConfiguration
     textSpan.children.forEach((f) {
